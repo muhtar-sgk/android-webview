2ec6c1a557f7c278649f504e1e2c51e16ee7668c
muhtarA <muhtarrn@gmail.com>
2017-10-08 21:31:20 +0700

    first commit

---
+++ app/src/main/java/com/sirseni/addingnavigationbackfeaturetoandroidwebview/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.sirseni.addingnavigationbackfeaturetoandroidwebview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by muhtar on 10/4/17.
+ */
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
+            }
+        },2000);
+
+    }
+}
